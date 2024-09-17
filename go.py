7e2f017d49a68935f218de664c81eb17fafbc491
ming <1476542669@qq.com>
2024-09-17 16:22:47 +0800

    添加填写功能与提交功能

--- go.py
+++ go.py
@@ -1,14 +1,33 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-# 指定 msedgedriver.exe 的路径
+# 配置 Edge 浏览器驱动
 service = Service(executable_path='msedgedriver.exe')
-
-# 使用 Service 来启动 Edge 浏览器
 driver = webdriver.Edge(service=service)
 
-# 打开一个网页
-driver.get("https://www.google.com")
+try:
+    driver.start_client()
+    time.sleep(1)
+    # start_time = time.perf_counter()
+    driver.get("https://wj.qq.com/s2/15350207/5331/")
 
-# 关闭浏览器
-driver.quit()
+    input_field = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "inputs-input"))
+    )
+    input_field.send_keys("测试员1")
+
+    submit_button = driver.find_element(By.CLASS_NAME, "btn-submit")
+    submit_button.click()
+
+    # end_time = time.perf_counter()
+    # elapsed_time = end_time - start_time
+    # print(f"脚本运行时间: {elapsed_time} 秒")
+
+finally:
+    time.sleep(5)
+    driver.quit()
