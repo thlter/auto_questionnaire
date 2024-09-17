9a39ce53de15c7399abc19792dcc22bfbb7f4c7f
ming <1476542669@qq.com>
2024-09-17 18:35:43 +0800

    添加多项单选题能力

--- go.py
+++ go.py
@@ -12,14 +12,20 @@ driver = webdriver.Edge(service=service)
 
 try:
     driver.start_client()
-    time.sleep(1)
     # start_time = time.perf_counter()
     driver.get("https://wj.qq.com/s2/15350207/5331/")
 
-    input_field = WebDriverWait(driver, 5).until(
+    # 填空题
+    input_field = WebDriverWait(driver, 30).until(
         EC.presence_of_element_located((By.CLASS_NAME, "inputs-input"))
     )
     input_field.send_keys("测试员1")
+
+    # 多项单选题
+    option_1 = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "label[for='q-7-AQyw-o-1-ytdT']"))
+    )
+    option_1.click()
 
     submit_button = driver.find_element(By.CLASS_NAME, "btn-submit")
     submit_button.click()
@@ -28,6 +34,11 @@ try:
     # elapsed_time = end_time - start_time
     # print(f"脚本运行时间: {elapsed_time} 秒")
 
+# except :
+#     print("执行错误")
+# else:
+#     print("成功")
+
 finally:
-    time.sleep(5)
+    time.sleep(60)
     driver.quit()
