f657558acb886237add6a6e1503fd32b91e1c01c
ming <1476542669@qq.com>
2024-09-18 23:59:32 +0800

    增加多个同种题型

--- go.py
+++ go.py
@@ -9,36 +9,66 @@ from selenium.webdriver.support import expected_conditions as EC
 # 配置 Edge 浏览器驱动
 service = Service(executable_path='msedgedriver.exe')
 driver = webdriver.Edge(service=service)
+# number = 0
 
 try:
     driver.start_client()
-    # start_time = time.perf_counter()
     driver.get("https://wj.qq.com/s2/15350207/5331/")
 
-    # 填空题
-    input_field = WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "inputs-input"))
+    # 第一题 单选题
+    option_1 = WebDriverWait(driver, 60).until(
+        EC.presence_of_element_located((By.XPATH, "//p[text()='是']"))
     )
-    input_field.send_keys("测试员1")
-
-    # 多项单选题
-    option_1 = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//p[text()='222']"))
-    )
+    # start_time = time.perf_counter()
     option_1.click()
+    # number += 1
 
-    # 多级联动题
-    link_option = driver.find_element(By.XPATH, "//span[text()='电饭锅']")
-    link_option.click()
-    link_option = driver.find_element(By.XPATH, "//span[text()='瓦尔哈拉']")
-    link_option.click()
-    link_option = driver.find_element(By.XPATH, "//span[text()='二万人']")
-    link_option.click()
+    # 第二题 图片选择题
+    option_1 = driver.find_element(By.XPATH, "//p[text()='C']")
+    option_1.click()
+    # number += 1
+
+    # 第三题 单选题
+    option_1 = driver.find_element(By.XPATH, "//p[text()='222']")
+    option_1.click()
+    # number += 1
+
+    # 第四题 多级联动题
+    option_1 = driver.find_element(By.XPATH, "//span[text()='电饭锅']")
+    option_1.click()
+    option_1 = driver.find_element(By.XPATH, "//span[text()='瓦尔哈拉']")
+    option_1.click()
+    option_1 = driver.find_element(By.XPATH, "//span[text()='二万人']")
+    option_1.click()
+    # number += 1
+
+    # 第五题 填空题
+    input_field = driver.find_element(By.CSS_SELECTOR, "input[placeholder='请输入地名']")
+    input_field.send_keys("欧洲")
+    # number += 1
+
+    # 第六题 多级联动题
+    option_1 = driver.find_element(By.XPATH, "//span[text()='欧克']")
+    option_1.click()
+    option_1 = driver.find_element(By.XPATH, "//span[text()='风格化']")
+    option_1.click()
+    # number += 1
+
+    # 第七题 填空题
+    input_field = driver.find_element(By.CSS_SELECTOR, "input[placeholder='请输入中文']")
+    input_field.send_keys("不知道")
+    # number += 1
+
+    # 第八题 单选题
+    option_1 = driver.find_element(By.XPATH, "//p[text()='大学本科']")
+    option_1.click()
+    # number += 1
 
     # 选择同意协议
     check_box = driver.find_element(By.CLASS_NAME, "t-checkbox__input")
     check_box.click()
 
+    # 提交问卷
     submit_button = driver.find_element(By.CLASS_NAME, "btn-submit")
     submit_button.click()
 
@@ -47,7 +77,7 @@ try:
     # print(f"脚本运行时间: {elapsed_time} 秒")
 
 # except :
-#     print("执行错误")
+#     print(f"执行错误{number}")
 # else:
 #     print("成功")
 
